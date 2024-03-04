c0bec4d28b7cb1f4247baaa7bce56b6b133ee706
Hoang Kiem <kiemhd@outlook.com>
2024-03-04 09:59:11 +0700

    chore: clean

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     @stack('style')
-    <link rel="stylesheet" href="{{ assets('css/styles.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
     <title>{{ isset($title) ? $title : 'New Document' }}</title>
 </head>
 <body>
