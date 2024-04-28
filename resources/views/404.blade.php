ea83e173de2397fab8a6a4a96669487e253336b7
Hoang Kiem <kiemhd@outlook.com>
2024-04-29 03:13:58 +0700

    chore: add document title to available pages

--- resources/views/404.blade.php
+++ resources/views/404.blade.php
@@ -1,4 +1,5 @@
 @extends('layouts.app')
+@section('title', '404 Not found')
 @section('content')
 <h1>404</h1>
 @endsection
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,4 +1,5 @@
 @extends('layouts.app')
+@section('title', 'Home')
 @section('content')
     <h1>Hello World</h1>
     @php
