36ee87c1b8d166fb2a50aadf7e20274e32317bbb
Hoang Kiem <kiemhd@outlook.com>
2024-04-29 02:58:39 +0700

    chore: update section title for app

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     @stack('style')
     <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
-    <title>{{ isset($title) ? $title : 'New Document' }}</title>
+    <title>@yield('title')</title>
 </head>
 <body>
     <main>
